4c7b7c9d4887c4eb613456c1d8aff04503abedf2
Katherine Milagros Quispe Pantoja <katherinebemol17@gmail.com>
2023-02-02 15:45:12 -0500

    Adding the buttons to the application

--- src/App.js
+++ src/App.js
@@ -16,13 +16,28 @@ function App() {
       <div className='calculator-container'>
         <div className='file'>
           <Boton>1</Boton>
-          <Boton>1</Boton>
-          <Boton>1</Boton>
+          <Boton>2</Boton>
+          <Boton>3</Boton>
           <Boton>+</Boton>
         </div>
-        <div className='file'></div>
-        <div className='file'></div>
-        <div className='file'></div>
+        <div className='file'>
+          <Boton>4</Boton>
+          <Boton>5</Boton>
+          <Boton>6</Boton>
+          <Boton>-</Boton>
+        </div>
+        <div className='file'>
+          <Boton>7</Boton>
+          <Boton>8</Boton>
+          <Boton>9</Boton>
+          <Boton>*</Boton>
+        </div>
+        <div className='file'>
+          <Boton>=</Boton>
+          <Boton>0</Boton>
+          <Boton>.</Boton>
+          <Boton>/</Boton>
+        </div>
         <div className='file'></div>
       </div>
     </div>
--- src/components/Boton.js
+++ src/components/Boton.js
@@ -1,13 +1,15 @@
 import React from "react";
+import './Boton.css'
 
 function Boton(props) {
 
 	const isOperator = valor => {
 		return isNaN(valor) && (valor !== '.') && (valor !== '=')
+		/* no es un operador si contiene vacio,punto ó igual */
 	}
 
 	return(
-		<div className={`container-boton ${isOperator(props.children) ? 'operador' : ''}`.trimEnd()}>
+		<div className={`container-boton ${isOperator(props.children) ? 'operator' : ''}`.trimEnd()}>
 			{props.children}
 		</div>
 	)
